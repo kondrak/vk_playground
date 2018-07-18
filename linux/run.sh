8679e7e853ced8a7f0773eff0e8f6037c6192ac5
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2018-07-18 07:26:55 -0700

    Added build targets for Linux.

--- linux/run.sh
+++ linux/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LD_LIBRARY_PATH=$VULKAN_SDK/x86_64/lib VK_LAYER_PATH=$VULKAN_SDK/x86_x64/etc/explicit_layer.d ./VkPlayground
+LD_LIBRARY_PATH=$VULKAN_SDK/x86_64/lib VK_LAYER_PATH=$VULKAN_SDK/x86_64/etc/explicit_layer.d ./VkPlayground
